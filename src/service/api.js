779ef70a1d55b6f24b0f1dff77283aa32d743564
열일 <isawthat77@naver.com>
2022-05-18 15:56:42 +0900

    API 호출 코드 작성

--- src/service/api.js
+++ src/service/api.js
@@ -1,3 +1,9 @@
-export const api = () => {
-
+const API_URL = 'https://randomuser.me/api/?results=50&noinfo'
+export const api = async() => {
+    try{
+        const res = await fetch(API_URL)
+        if(res.ok) return res.json()
+    }catch(e){
+        throw new Error(`에러 발생! ${e.message}, ${e.status}`)
+    }
 }
